29bb45cfac35022a710cb9869ff9bd3ed20a23bc
Lance Pidor <kyrios213@gmail.com>
2021-04-20 12:03:37 +0800

    Added profiles and media

--- til/urls.py
+++ til/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from feed import urls as feed_urls
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', include(feed_urls, namespace="feed")),
     url("", include("allauth.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
